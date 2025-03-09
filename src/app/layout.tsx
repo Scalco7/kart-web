17752cd56dda887e39d5ec6d905128160d4a7002
Felipe Scalco <fscalco7@gmail.com>
2025-03-09 18:26:56 -0300

    navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 
 const poppins = Poppins({
-  weight: ['600'],
+  weight: ['400', '500', '600'],
   subsets: ['latin'],
   display: 'swap',
 })
--- src/components/atoms/header/header.tsx
+++ src/components/atoms/header/header.tsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
 import styles from "./header.module.css";
 
-export default function Header(){
+export default function Header() {
+    const [showNavigationBox, setShowNavigationBox] = useState(false)
+
+    function toogleShowNavigationBox(){
+        setShowNavigationBox(!showNavigationBox)
+    }
+
     return <main className={styles.main}>
-        <img className={styles.img} src="kart-logo.png"/>
+        <img className={styles.hamburguerIcon} src="icons/hamburguer_icon.png" onClick={() => toogleShowNavigationBox()}/>
+        <div className={`${styles.navigationBox} ${showNavigationBox ? styles.visible : styles.hidden}`}>
+            <a href="/" className={styles.navigationText}>Home</a>
+            <div className={styles.line}></div>
+            <a href="/races" className={styles.navigationText}>Corridas</a>
+        </div>
+        <nav className={styles.navigationBar}>
+            <a href="/" className={styles.navigationText}>Home</a>
+            <a href="/races" className={styles.navigationText}>Corridas</a>
+        </nav>
+        <img className={styles.img} src="kart-logo.png" />
     </main>
 }
